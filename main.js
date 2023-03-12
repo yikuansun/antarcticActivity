b0969ffa5a3bbb4d25a354f7512d867dc47ce40b
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-03-12 17:50:52 -0400

    Stop using electron-tray-window
    Sorr

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const TrayWindow = require("electron-tray-window");
 
 const { ipcMain, Tray, app, BrowserWindow } = require("electron");
 const path = require("path");
+require("@electron/remote/main").initialize();
 
 app.on("ready", () => {
     var timeout = 10;
     if (process.platform === "linux") {
-        timeout = 500;
+        timeout = 200;
     }
     setTimeout(function () {
         var mainWindow = new BrowserWindow({
@@ -14,18 +14,18 @@ app.on("ready", () => {
             height: 400,
             backgroundColor: "#222222",
             frame: false,
+            alwaysOnTop: true,
+            show: true,
+            useContentSize: true,
             webPreferences: {
                 nodeIntegration: true,
                 enableRemoteModule: true,
                 contextIsolation: false,
+                backgroundThrottling: false,
             },
         });
         mainWindow.loadFile(path.join(__dirname, "window/index.html"));
-        require("@electron/remote/main").enable(mainWindow);
+        require("@electron/remote/main").enable(mainWindow.webContents);
 
-        TrayWindow.setOptions({
-            trayIconPath: "window/icon.png",
-            window: mainWindow,
-        });
     }, timeout);
 });
