36710c3a6b67cb6736f21b903e2f27a640d01c55
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-03-12 18:42:18 -0400

    Create user data file

--- window/app.js
+++ window/app.js
@@ -1,3 +1,10 @@
+var fs = require("fs");
+var path = require("path");
+var { app } = require("@electron/remote");
+
+var saveFilePath = path.join(app.getPath("userData"), "saveFile.json");
+if (!fs.existsSync(saveFilePath)) fs.writeFileSync(saveFilePath, "{}");
+
 var startupTime = (new Date()).getTime();
 
 function getScreentime() {
