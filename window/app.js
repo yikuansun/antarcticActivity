7426732ef888b6f53834707599180aa47fe51c51
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-04-08 19:57:27 -0400

    Save day & week data

--- window/app.js
+++ window/app.js
@@ -3,15 +3,49 @@ var path = require("path");
 var { app } = require("@electron/remote");
 var { ipcRenderer } = require("electron");
 
+function getWeekStart(d=new Date()) {
+    var current = new Date();
+    var first = current.getDate() - current.getDay();
+    var weekFirstDay = new Date(current.setDate(first));
+    return weekFirstDay;
+}
+
 var saveFilePath = path.join(app.getPath("userData"), "saveFile.json");
-if (!fs.existsSync(saveFilePath)) fs.writeFileSync(saveFilePath, "[]");
+if (!fs.existsSync(saveFilePath)) fs.writeFileSync(saveFilePath, JSON.stringify({
+    weeks: {},
+    days: {},
+    sessions: {},
+}));
 var saveFileData = JSON.parse(fs.readFileSync(saveFilePath));
 
 var currentSessionData = {
-    date: (new Date()).toString(),
+    date: (new Date()).toDateString(),
     timeElapsed: 0,
 };
-saveFileData.push(currentSessionData);
+saveFileData.sessions[(new Date()).getTime()] = currentSessionData;
+var currentDayData, currentWeekData;
+if (saveFileData.days[(new Date()).toDateString()]) {
+    currentDayData = saveFileData.days[(new Date()).toDateString()];
+}
+else {
+    currentDayData = {
+        date: (new Date()).toDateString(),
+        timeElapsed: 0,
+    };
+    saveFileData.days[(getWeekStart(new Date())).toDateString()] = currentDayData;
+    fs.writeFileSync(saveFilePath, JSON.stringify(saveFileData));
+}
+if (saveFileData.weeks[(getWeekStart(new Date())).toDateString()]) {
+    currentWeekData = saveFileData.weeks[(getWeekStart(new Date())).toDateString()];
+}
+else {
+    currentWeekData = {
+        startDate: (getWeekStart(new Date())).toDateString(),
+        timeElapsed: 0,
+    };
+    saveFileData.weeks[(getWeekStart(new Date())).toDateString()] = currentWeekData;
+    fs.writeFileSync(saveFilePath, JSON.stringify(saveFileData));
+}
 
 var startupTime = (new Date()).getTime();
 
